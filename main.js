195762606b030cf6de1f879a9f75f77514661821
Radouane-prog <hadj.radouane1@gmail.com>
2026-01-24 21:12:57 +0100

    Feat: récupération des éléments du DOM pour la partie pomodoro.
    J'ai aussi mis des input de type number dans les settings au lieu des p.

---
+++ main.js
@@ -0,0 +1,53 @@
+let buttonSettings = document.getElementById("button_settings");
+let containerTimer = document.getElementById("timer");
+let textMin = document.getElementById("min");
+let textSec = document.getElementById("sec");
+let arrayTrophies = document.getElementsByClassName("trophy");
+let textMotivation = document.getElementById("text_motivation");
+let buttonStart = document.getElementById("button_start");
+
+//récupération des éléments du DOM Settings
+
+let containerSettings = document.getElementById("container_settings");
+
+let arrowUpWorkMin = document.getElementById("arrow_up_work_min");
+let arrowDownWorkMin = document.getElementById("arrow_down_work_min");
+let arrowUpWorkSec = document.getElementById("arrow_up_work_sec");
+let arrowDownWorkSec = document.getElementById("arrow_down_work_sec");
+
+let inputWorkMin = document.getElementById("work_min");
+let inputWorkSec = document.getElementById("work_sec");
+
+let arrowUpShortMin = document.getElementById("arrow_up_short_min");
+let arrowDownShortMin = document.getElementById("arrow_down_short_min");
+let arrowUpShortSec = document.getElementById("arrow_up_short_sec");
+let arrowDownShortSec = document.getElementById("arrow_down_short_sec");
+
+let inputShortMin = document.getElementById("short_min");
+let inputSshortSec = document.getElementById("short_sec");
+
+let arrowUpLongMin = document.getElementById("arrow_up_long_min");
+let arrowDownLongMin = document.getElementById("arrow_down_long_min");
+let arrowUpLongSec = document.getElementById("arrow_up_long_sec");
+let arrowDownLongSec = document.getElementById("arrow_down_long_sec");
+
+let inputLongMin = document.getElementById("long_min");
+let inputLongSec = document.getElementById("long_sec");
+
+let arrowUpCycles = document.getElementById("arrow_up_cycles");
+let arrowDownCycles = document.getElementById("arrow_down_cycles");
+
+let inputCycles= document.getElementById("cycles");
+
+let buttonSaveSettings = document.getElementById("save_setting");
+let buttonCancelSettings = document.getElementById("cancel_setting");
+
+//récupération des éléments du DOM de la pop-up
+
+let containerAvertissement = document.getElementById("container_avertissement");
+let buttonDeleteTimer = document.getElementById("button_delete_timer");
+let buttonKeepTimer = document.getElementById("button_keep_timer");
+
+
+
+
